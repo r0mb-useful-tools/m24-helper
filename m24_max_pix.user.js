cb06cfc87dddcece58af426d18502c414bb37cb7
r0mb-useful-tools <biowit@gmail.com>
2026-09-03 19:43:31 +0300

    Update m24_max_pix.user.js to v2.0

--- m24_max_pix.user.js
+++ m24_max_pix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         m24_max_pix
+// @name         Заглушки для МАКСа
 // @namespace    https://web.max.ru/
-// @version      1.9
+// @version      2.0
 // @author       Roman Balaev
 // @description  Отправляет автора из заглушек автоматом (только web.max.ru)
 // @match        https://web.max.ru/*
